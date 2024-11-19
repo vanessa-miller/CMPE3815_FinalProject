8d2bdf8904b568fa250e628cc85e3b58c349b5f5
vanessa-miller <vanessamiller101@gmail.com>
2024-11-19 17:27:05 -0500

    Update FinalProject.ino to have flex sensor code

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -1,12 +1,34 @@
 /*
-project
+MICROCONTROLLERS LAB X - FINAL PROJECT
+using flex sensors to measure hand movement and motors to move fingers to 
+replicate the hand's movement.
 */
-void setup() {
-  // put your setup code here, to run once:
 
+const int flexPIN = A0; // output from flex sensor voltage divider
+
+const float VCC = 5.00; // voltage
+const float R_DIV = 10000.0; // resistance of resistor
+
+const float straightResist = 26000.0; // resistance when straight
+const float bentResist = 51000.0; // resistance at 90 deg
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(flexPIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read the ADC, and calculate voltage and resistance from it
+  int flexADC = analogRead(flexPIN);
+  float flexV = flexADC * VCC / 1023.0;
+  float flexR = R_DIV * (VCC / flexV - 1.0);
+  Serial.println("Resistance: " + String(flexR) + " ohms");
 
+  // Use the calculated resistance to estimate the sensor's
+  // bend angle:
+  float angle = map(flexR, straightResist, bentResist, 0, 90.0);
+  Serial.println("Bend: " + String(angle) + " degrees");
+  Serial.println();
+
+  delay(2000);
 }
