627add4ffea54ddce775458518a4535827872a29
vanessa-miller <vanessamiller101@gmail.com>
2024-12-03 20:24:04 -0500

    Update FinalProject.ino

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -11,10 +11,10 @@ Servo myServo1; // servo for index
 Servo myServo2; // servo for middle
 Servo myServo3; // servo for ring
 Servo myServo4; // servo for pinky
-const int servoPIN1 = 3;
-const int servoPIN2 = 4;
-const int servoPIN3 = 5;
-const int servoPIN4 = 6;
+const int servoPIN1 = 8;
+const int servoPIN2 = 9;
+const int servoPIN3 = 10;
+const int servoPIN4 = 11;
 
 const int flexPIN1 = A0; // output from flex sensor voltage divider for index
 const int flexPIN2 = A1; // output from flex sensor voltage divider for middle
@@ -23,14 +23,14 @@ const int flexPIN4 = A3; // output from flex sensor voltage divider for pinky
 
 const float R_DIV = 10000.0; // resistance of resistor
 
-const float straightR1 = 26000.0; // resistance when straight (index)
-const float bentR1 = 51000.0; // resistance at 90 deg (index)
-const float straightR2 = 26000.0; // resistance when straight (middle)
-const float bentR2 = 51000.0; // resistance at 90 deg (middle)
-const float straightR3 = 26000.0; // resistance when straight (ring)
-const float bentR3 = 51000.0; // resistance at 90 deg (ring)
-const float straightR4 = 26000.0; // resistance when straight (pinky)
-const float bentR4 = 51000.0; // resistance at 90 deg (pinky)
+const float straightR1 = 12988.76; // resistance when straight (index)
+const float bentR1 = 24328.86; // resistance at 90 deg (index)
+const float straightR2 = 1126666.80; // resistance when straight (middle)
+const float bentR2 = 2035999.90; // resistance at 90 deg (middle)
+const float straightR3 = 1695000.00; // resistance when straight (ring)
+const float bentR3 = 152380.94; // resistance at 90 deg (ring)
+const float straightR4 = 29.41; // resistance when straight (pinky)
+const float bentR4 = 29.41; // resistance at 90 deg (pinky)
 
 //==========================================================================
 
@@ -69,20 +69,25 @@ void fingerAngle(int finger, int flexPIN, float straightResist, float bentResist
   Serial.println();
   
   // move the servo to move the robot finger
-  float mappedAngle = (angle * -1)*1.3;
-  Serial.println(mappedAngle);
-  if (finger == 1) myServo1.write(mappedAngle);
-  if (finger == 2) myServo2.write(mappedAngle);
-  if (finger == 3) myServo3.write(mappedAngle);
-  if (finger == 4) myServo4.write(mappedAngle);
+  //if (finger == 1) myServo1.write(angle*2.43-12);
+  //if (finger == 2) myServo2.write(angle);
+  //if (finger == 3) myServo3.write(angle);
+  //if (finger == 4) myServo4.write(angle);
 }
 
 //==========================================================================
 
 void loop() {
+  
   fingerAngle(1, flexPIN1, straightR1, bentR1);
   fingerAngle(2, flexPIN2, straightR2, bentR2);
   fingerAngle(3, flexPIN3, straightR3, bentR3);
   fingerAngle(4, flexPIN4, straightR4, bentR4);
+  
+
+
+
+  delay(5000);
+
 
 }
